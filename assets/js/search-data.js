7fcf749e80fb5d6a7e3ff2b2d77763ee5e5fc095
jindal013 <56328713+jindal013@users.noreply.github.com>
2025-09-04 02:39:10 +0000

    Deploying to gh-pages from @ jindal013/jaxformer-website-v2@81021f3e98c48436b2895c73a35841ed56c678e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-how-to-scale-your-model",
+    title: "How to Scale Your Model",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
