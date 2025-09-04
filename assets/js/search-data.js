d026aaace09408166d1b7a8ebfbb288920fea9f5
jindal013 <56328713+jindal013@users.noreply.github.com>
2025-09-04 07:45:31 +0000

    Deploying to gh-pages from @ jindal013/jaxformer-website-v2@2b084b2379042ebaec8a0df5a49c11f49e13904b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,55 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/tokenization/";
               },
+            },{id: "dropdown-part-2-main-model",
+              title: "Part 2. Main Model",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/main_model/";
+              },
+            },{id: "dropdown-part-3-distributed-training",
+              title: "Part 3. Distributed Training",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/distributed/";
+              },
+            },{id: "dropdown-part-4-dataset-amp-config",
+              title: "Part 4. Dataset &amp; Config",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/dataset/";
+              },
+            },{id: "dropdown-part-5-main-training-loop",
+              title: "Part 5. Main Training Loop",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/main_loop/";
+              },
+            },{id: "dropdown-part-6-moe-implementation",
+              title: "Part 6. MoE Implementation",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/moe/";
+              },
+            },{id: "dropdown-part-7-training-results",
+              title: "Part 7. Training Results",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/training/";
+              },
+            },{id: "dropdown-part-8-conclusion",
+              title: "Part 8. Conclusion",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/conclusion/";
+              },
             },{
       id: 'light-theme',
       title: 'Change theme to light',
