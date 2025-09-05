02eb11d1e61942c67954e2a270f89de27f8e2359
jindal013 <56328713+jindal013@users.noreply.github.com>
2025-09-05 01:22:45 +0000

    Deploying to gh-pages from @ jindal013/jaxformer-website-v2@d4acfb70dee2264d0b89f45edf9800ec5f8fcdb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/tokenization/";
               },
-            },{id: "dropdown-part-2-main-model",
-              title: "Part 2. Main Model",
+            },{id: "dropdown-part-2-base-model",
+              title: "Part 2. Base Model",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/main_model/";
+                window.location.href = "/base_model/";
               },
-            },{id: "dropdown-part-3-distributed-training",
-              title: "Part 3. Distributed Training",
+            },{id: "dropdown-part-3-sharded-model",
+              title: "Part 3. Sharded Model",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/distributed/";
+                window.location.href = "/sharded/";
               },
             },{id: "dropdown-part-4-dataset-amp-config",
               title: "Part 4. Dataset &amp; Config",
@@ -44,15 +44,15 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/dataset/";
               },
-            },{id: "dropdown-part-5-main-training-loop",
-              title: "Part 5. Main Training Loop",
+            },{id: "dropdown-part-5-distributed-training",
+              title: "Part 5. Distributed Training",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/main_loop/";
+                window.location.href = "/distributed_training/";
               },
-            },{id: "dropdown-part-6-moe-implementation",
-              title: "Part 6. MoE Implementation",
+            },{id: "dropdown-part-6-mixture-of-experts",
+              title: "Part 6. Mixture of Experts",
               description: "",
               section: "Dropdown",
               handler: () => {
